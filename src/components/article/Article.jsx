c2fbc78653b369248a5b455c87dd3aea37d07f2c
Md Minhaz Uddin <minhazalam754@gmail.com>
2023-11-02 20:01:45 +0600

    blog is added

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./article.css";
-const Article = () => {
+const Article = ({ imgUrl, title, date }) => {
   return (
-    <div>
-      <h1>This is Article Section</h1>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+          <p>Read Full Article</p>
+        </div>
+      </div>
     </div>
   );
 };
